b1db871c6cff282d9c654c763df6e656b57333e8
kashishj197 <kashishj197@gmail.com>
2020-04-14 13:46:12 +0530

    integrated state_district_wise api using node

---
+++ covidstats/backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const https = require('https');
+const app = express();
+
+app.use("/api/stats",(req, res, next) => {
+  let data = '';
+  https.get('https://api.covid19india.org/v2/state_district_wise.json', (resp) => {
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      res.status(200).json({data});
+    });
+  }).on("error", (err) => {
+  console.log("Error: " + err.message);
+  });
+  // console.log('hello');
+});
+
+module.exports = app;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 import { NavbarAppComponent } from './navbar/navbar.component';
 import { WallsliderAppComponent } from './wallslider/wallslider.component';
+import { GraphAppComponent } from './graph/graph.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarAppComponent,
-    WallsliderAppComponent
+    WallsliderAppComponent,
+    GraphAppComponent
   ],
   imports: [
     BrowserModule,
